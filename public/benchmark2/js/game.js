06dcc09335020eeb0d6353f345e2c1ae2cce1526
kwakuoowusu <kwakuoowusu@gmail.com>
2017-04-01 23:01:42 -0400

    set up skeleton for the game

--- public/benchmark2/js/game.js
+++ public/benchmark2/js/game.js
@@ -1,1 +1,13 @@
-hello
+var AquaCycle = AquaCycle || {};
+
+TopDownGame.Game = function(){};
+
+TopDownGame.Game.prototype = {
+        create: function(){
+            //load first level this.map = this.game.add.tilemap('levl1');
+        },
+
+        update: function(){
+
+        }
+}
