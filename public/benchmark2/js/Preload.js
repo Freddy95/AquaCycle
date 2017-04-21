ae4148446362eea3fbdc8972ba01f8c5b5a96157
Devon Maguire <maguiredm@gmail.com>
2017-04-21 11:59:11 -0400

    Starting to reorganize files and clean up the code.

--- public/benchmark2/js/Preload.js
+++ public/benchmark2/js/Preload.js
@@ -4,21 +4,29 @@ AquaCycle.Preload = function(){};
 
 AquaCycle.Preload.prototype = {
     preload: function(){
-        AquaCycle.game.load.spritesheet('player', '../assets/SharkSpriteSheet2.png',128,64);
-        AquaCycle.game.load.image('shipwreck','../assets/shipwreck.png');
+        // Load the levels of the game and their assets
         AquaCycle.game.load.tilemap('level1','../assets/level1.json',null,Phaser.Tilemap.TILED_JSON);
-        AquaCycle.game.load.image('world','../assets/DepthTileMockups.png');
-        AquaCycle.game.load.image('world1','../assets/GIMP/DepthTileMockups1.png');
+        AquaCycle.game.load.image('world1','../assets/DepthTileMockups.png');
         AquaCycle.game.load.image('world2','../assets/Tiled/DepthTileMockups.png');
-        AquaCycle.game.load.image('shark','../assets/shark.png');
-        AquaCycle.game.load.image('sanddollar', '../assets/sanddollar.png');
-        AquaCycle.game.load.image('conchshell', '../assets/conchshell.png');
-        AquaCycle.game.load.image('seagrass', '../assets/seagrass.png');
+        AquaCycle.game.load.image('infobox','../assets/infobox.png');
+        AquaCycle.game.load.image('heart','../assets/heart.png');
+
+        // Load the player sprite sheets
+        AquaCycle.game.load.spritesheet('player', '../assets/SharkSpriteSheet2.png',128,64);
+
+        // Load the predator sprite sheets
+        AquaCycle.game.load.spritesheet('predator','../assets/LargerEnemySpriteSheet.png',192,96);
+
+        // Load the prey sprite sheets
         AquaCycle.game.load.image('pinkshrimp','../assets/pinkshrimp.png');
         AquaCycle.game.load.image('brownshrimp','../assets/brownshrimp.png');
-        AquaCycle.game.load.spritesheet('predator','../assets/LargerEnemySpriteSheet.png',192,96);
-        AquaCycle.game.load.image('infobox','../assets/infobox2.png');
-        AquaCycle.game.load.image('heart','../assets/heart.png');
+
+        // Load the item images
+        AquaCycle.game.load.image('shipwreck','../assets/shipwreck.png');
+        AquaCycle.game.load.image('seagrass', '../assets/seagrass.png');
+        AquaCycle.game.load.image('sanddollar', '../assets/sanddollar.png');
+        AquaCycle.game.load.image('conchshell', '../assets/conchshell.png');
+        AquaCycle.game.load.image('shark','../assets/shark.png');
     },
 
     create: function() {
